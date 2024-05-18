fb2425d37c9b6dc089b329defdf91d7cec91944d
nathanansel28 <nathanansel28@gmail.com>
2024-05-19 04:46:19 +0800

    first commit

---
+++ 0 Sample LinProg.py
@@ -0,0 +1,18 @@
+import scipy
+from scipy.optimize import linprog
+
+#TO MAXIMIZE Z
+
+obj = [-20, -12, -40, -25]
+
+lhs_ineq = [[1, 1, 1, 1],  # Manpower
+            [3, 2, 1, 0],  # Material A
+            [0, 1, 2, 3]]  # Material B
+
+rhs_ineq = [ 50,  # Manpower
+            100,  # Material A
+             90]  # Material B
+
+opt = linprog(c=obj, A_ub=lhs_ineq, b_ub=rhs_ineq,
+              method="revised simplex")
+opt
---
+++ Scenario 2.py
@@ -0,0 +1,61 @@
+import scipy
+from scipy.optimize import linprog
+import pandas as pd
+
+#ALL THE DATA (NEW)
+df = pd.read_excel('Data.xlsx', sheet_name='Scenario2')
+t = df.iloc[:,0]
+D = df.iloc[:,1]
+LCA1 = df.iloc[:,2]
+LCA2 = df.iloc[:,3]
+LCA3 = df.iloc[:,4]
+c1 = df.iloc[:,5]
+c2 = df.iloc[:,6]
+c3 = df.iloc[:,7]
+
+# PART 4: EXECUTION
+n = len(t)
+Solution_Matrix = []
+
+for i in range(n):
+    # t1 = t[i]
+    L1 = LCA1[i]
+    L2 = LCA2[i]
+    L3 = LCA2[i]
+    Demand = D[i]
+    C1 = -1 * c1[i]
+    C2 = -1 * c2[i]
+    C3 = -1 * c3[i] 
+
+    # PART 1: CO2 FUNCTION
+    coeff_CO2 = [L1, L2, L3]
+
+    # PART 2: INEQUALITIES
+    # ICE, EV, then H
+    lhs_ineq = [
+        [-1, 0, 0],
+        [0, -1, 0],
+        [0, 0, -1],
+        [0, -3, 7]
+    ]
+    rhs_ineq = [
+        C1,
+        C2,
+        C3,
+        0
+    ]
+
+    # PART 3: EQUALITIES
+    lhs_eq = [[1, 1, 1]]
+    rhs_eq = [Demand]
+
+    opt = linprog(c=coeff_CO2, A_ub=lhs_ineq, b_ub=rhs_ineq, A_eq=lhs_eq, b_eq=rhs_eq, method="highs")
+    Solution_Matrix.append(list(opt.x))
+
+# print(Solution_Matrix)
+
+# for x in Solution_Matrix:
+#     print(', '.join([str(n) for n in x]))
+
+for x in Solution_Matrix:
+    print('\t'.join([str(n) for n in x]))
